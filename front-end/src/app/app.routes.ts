eb063753d48f0cc0c3cd62eb6fec4607d82be5a9
jvadillo88 <julian.vadillo.a@gmail.com>
2025-05-02 12:59:01 +0200

    Ajout des fonctionnalités pour changer mdp utilisateur ainsi que modifs sur server.js
    # Conflicts:
    #	NodeJs/server.js

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,8 +1,14 @@
+// app.routes.ts
+
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardProjectsComponent } from './dashboard-projects/dashboard-projects.component';
 import { DashboardFoldersComponent } from './dashboard-folders/dashboard-folders.component';
 import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component'; // ✅ Import du composant
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+
+
 
 // Définition des routes de l'application
 export const routes: Routes = [
@@ -16,5 +22,10 @@ export const routes: Routes = [
   { path: 'dashboard-projects', component: DashboardProjectsComponent },
 
   // Route vers le tableau de bord des dossiers
-  { path: 'dashboard-folders', component: DashboardFoldersComponent }
+  { path: 'dashboard-folders', component: DashboardFoldersComponent },
+
+  // ✅ Route pour la page "Mot de passe oublié"
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+
+  { path: 'reset-password', component: ResetPasswordComponent }
 ];
